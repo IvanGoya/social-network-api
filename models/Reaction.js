751e6e858f74ca3b3b47bf99324b38a759d97c23
Ivan Goya <ivan.goya98@gmail.com>
2022-06-28 13:55:42 -0400

    added the models files

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+const {Schema, model} = require("mongoose");
+
+const reactionSchema = new Schema(
+    {
+      reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+      },
+      reactionBody: {
+        type: String,
+        required: true,
+        max_length: 280,
+      },
+      username: [
+        {
+            type: String,
+            require: true
+        }
+      ],
+      createdAt: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+   
+    {
+      toJSON: {
+        getters: true,
+      },
+    }
+  );
+  
+  const Reaction = model('reaction', reactionSchema);
+
+
+module.exports = Reaction;
